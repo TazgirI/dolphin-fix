0e8d9a435b9d7b5a19f0c14f9bf927cc230f5fc4
TM316157 <TM316157@falmouth.ac.uk>
2025-04-13 23:54:35 +0100

    LivingIncomingDamage and player distance check implemented

--- src/main/java/net/tazgirl/dolphinfix/DolphinFix.java
+++ src/main/java/net/tazgirl/dolphinfix/DolphinFix.java
@@ -1,61 +1,73 @@
 package net.tazgirl.dolphinfix;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.Dolphin;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(DolphinFix.MODID)
 public class DolphinFix
 {
-    // Define mod id in a common place for everything to reference
+
     public static final String MODID = "dolphin_fix";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+
+
     public DolphinFix(IEventBus modEventBus, ModContainer modContainer)
     {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-
     }
 
 
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
+    public void onLivingHurt(LivingIncomingDamageEvent event)
     {
-
+        if (event.getEntity() instanceof Dolphin && event.getSource().is(DamageTypes.DROWN))
+        {
+            if (!isPlayerNearby(event.getEntity().level(), event.getEntity()))
+            {
+                if (event instanceof ICancellableEvent cancellableEvent)
+                {
+                    cancellableEvent.setCanceled(true);
+                }
+            }
+        }
     }
 
+    public static boolean isPlayerNearby(Level level, Entity entity)
+    {
+        MinecraftServer server = level.getServer();
+        int simulationDistance = server.getPlayerList().getSimulationDistance();
+
+        //The -15 is just to be safe and shouldn't affect gameplay
+        double rangeSqr = simulationDistance * simulationDistance * 15 - 15;
+
+
+        for (Player player : level.players())
+        {
+            if (player.distanceToSqr(entity) <= rangeSqr)
+            {
+                System.out.println(player.distanceToSqr(entity));
+                return true;
+            }
+            System.out.println(player.distanceToSqr(entity));
+        }
+
+        return false;
+    }
 
 }
